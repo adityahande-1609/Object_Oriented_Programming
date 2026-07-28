28dc1d69887f82e727f981bcc0aa95decc789aa4
adityahande-1609 <hande.adu@gmail.com>
2026-07-28 15:56:50 +0530

    Add files via upload

---
+++ inline functions.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+float pi =3.14;
+using namespace std; 
+inline int cube(int a)
+{
+	return a*a*a;
+}
+inline int lar(int a,int b)
+{
+	if(a<b){
+		return b;
+	}
+	else{
+		return a;
+	}
+}
+inline car(int a)
+{
+	float ar;
+	ar =pi*a*a;
+	return ar;
+}
+int main()
+{
+	float ar;
+	int a,a1,a2;
+	cout<<"enter number";
+	cin>>a;
+	cout<<" the cude is : "<<cube(a);
+	cout<<"\n\n\nEnter 2 numbers:";
+	cin>>a1>>a2;
+	cout<<"\nthe larger number is : "<<lar(a1,a2);
+	cout<<"\nenter the radius :";
+	cin>>a;
+	ar=car(a);
+	cout<<"\n Area of the circle is: "<<car(a); 
+	return 0;
+}
